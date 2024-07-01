d8520a33e145a91919b5eb13d785c618fa35ee2c
leo-prata <leo.prata@estudante.ufjf.br>
2024-07-01 14:10:36 -0300

    add login

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import CreateUserController from './controllers/user/CreateUserController';
+import AuthUserController from './controllers/user/AuthUserController';
 
 const router = Router();
 
 router.post('/users', CreateUserController.handle);
+router.post('/session', AuthUserController.handle);
 
 export { router };
